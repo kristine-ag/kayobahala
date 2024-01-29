46b59d7fcfa60ea1b98f7641a7f75dcd438bb2f5
Eiji-Kei <kristineangelagallawan9@gmail.com>
2024-01-29 19:22:18 +0800

    added node modules

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Gwapa ko")
+// console.log("Hello World!")
+
+const express = require ("express")
+const app = express ()
+
+const port = 8000
+app.listen (port, () => {
+    console.log (`Server connected to port  ${port}`)
+})
+
+app.get('/', (req, res) => {
+    res.send (`
+        <H1>
+            Hello World!
+        </H1>
+    `)
+
+    console.log("request received")
+})

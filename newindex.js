eb1a21150f58e5dbfc1d135450776e870c3e971a
Eiji-Kei <kristineangelagallawan9@gmail.com>
2024-03-06 18:42:17 +0800

    add express server

---
+++ newindex.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const port = 8000;
+
+app.use((req, res, next) => {
+    console.log('Request Made')
+    console.log(`Host: ${req.hostname}`)
+    console.log(`Path: ${req.path}`)
+    console.log(`Method: ${req.method}`)
+    next()
+})
+
+app.get('/', function (req, res) {
+//   res.send('Hello World')
+    res.sendFile(`./views/index.html`, {root: __dirname})
+})
+
+app.get('/about', function (req, res) {
+        res.sendFile(`./views/about.html`, {root: __dirname})
+})
+
+app.get('/home', (req, res) => {
+    res.redirect('/')
+})
+
+app.get('/aboutus', (req, res) => {
+    res.redirect('/about')
+})
+
+app.use((req, res) => {
+    res.status(404).sendFile(`./views/error.html`, {root: __dirname})
+})
+
+app.listen(port, () => {
+    console.log(`Server is Running On Port ${port}`)
+})
